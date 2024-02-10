c04274a3c20aed36272b136d86150b8f7f9d72a6
Rsyd <arkan1986muhammad@gmail.com>
2024-02-10 22:35:58 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -31,52 +31,8 @@ if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
 fi
 
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
-   	export TW_DEFAULT_LANGUAGE="en"
-	export OF_KEEP_FORCED_ENCRYPTION=1
-	export OF_PATCH_AVB20=1
-	export OF_USE_MAGISKBOOT=1
-	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
-	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
-	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
-	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
-	export OF_NO_MIUI_PATCH_WARNING=1
-	export OF_USE_GREEN_LED=0
-	export FOX_USE_BASH_SHELL=1
-export OF_ADVANCED_SECURITY=1
-	export FOX_ASH_IS_BASH=1
-	export FOX_USE_NANO_EDITOR=1
-	export FOX_USE_TAR_BINARY=1
-	export FOX_USE_ZIP_BINARY=1
-	export FOX_USE_SED_BINARY=1
-	export FOX_USE_XZ_UTILS=1
-	export FOX_REPLACE_BUSYBOX_PS=1
+        export OF_ADVANCED_SECURITY=1
 	export OF_USE_NEW_MAGISKBOOT=1
-	export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
-        export FOX_BUGGED_AOSP_ARB_WORKAROUND="1616300800"; # Sun 21 Mar 04:26:40 GMT 2021
-export OF_DONT_KEEP_LOG_HISTORY=1
-        # use system (ROM) fingerprint where available
-        export OF_USE_SYSTEM_FINGERPRINT=1
-export OF_NO_ADDITIONAL_MIUI_PROPS_CHECK=1
-	# OTA for custom ROMs
-        export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
-        export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
-        export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
-        export OF_QUICK_BACKUP_LIST="/boot;/data;/system_image;/vendor_image;"
-export FOX_BUILD_TYPE="Official"
-export FOX_BUILD="Official"
-	# Screen Settings
-	export ALLOW_DISABLE_NAVBAR=0
-	export OF_SCREEN_H=2246
-	export OF_STATUS_H=91
-	export OF_STATUS_INDENT_LEFT=48
-	export OF_STATUS_INDENT_RIGHT=48
-	export OF_CLOCK_POS=2
-	export OF_HIDE_NOTCH=1
-	# R12.1
-	export FOX_BUILD_TYPE="Beta"
-	export OF_MAINTAINER="Rsyd57"
-	export FOX_VERSION="R12.1"
- export FOX_VARIANT="SE"
 
 	# Magisk
 	export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk.zip
